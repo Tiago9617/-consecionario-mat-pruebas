134083f355f6060499ba668be4273c170ee8e8c4
tiago9617 <santiagorojas_1996@hotmail.com>
2023-10-08 20:24:32 -0500

    Creacion de Productos parte 2

--- consecionario-mat-pruebas/src/app/app.module.ts
+++ consecionario-mat-pruebas/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgComponent } from './components/img/img.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgComponent,
+    NavComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
--- consecionario-mat-pruebas/src/app/product.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Product{
-  image: string;
-  name: string;
-  price: number;
-  describe: string;
-}
